e899f542791b7e551198413b277d11515a2c599c
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-12-24 16:45:02 +0300

    feature: Добавить валидацию аватара

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const { errors } = require('celebrate');
 
 const auth = require('../middlewares/auth');
 
+const urlRegexp = /https?:\/\/w*\.?[1-9a-z-._~:/?#[\]@!$&'()*+,;=]{1,}#?/i;
+
 const getUserJoiValidation = () => celebrate({
   params: Joi.object()
     .keys({
@@ -21,6 +23,13 @@ const updateUserJoiValidation = () => celebrate({
 
 });
 
+const updateAvatarJoiValidation = () => celebrate({
+  body: Joi.object()
+    .keys({
+      avatar: Joi.string().pattern(urlRegexp),
+    }),
+});
+
 const {
   getUser, getUsers, updateUser, updateAvatar, getMe,
 } = require('../controllers/users');
@@ -35,6 +44,6 @@ users.get('/', getUsers);
 users.get('/me', getMe);
 users.patch('/me', updateUserJoiValidation(), updateUser);
 users.get('/:userId', getUserJoiValidation(), getUser);
-users.patch('/me/avatar', updateAvatar);
+users.patch('/me/avatar',updateAvatarJoiValidation(), updateAvatar);
 
 module.exports = users;
